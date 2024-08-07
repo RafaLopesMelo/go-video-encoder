796c876da68db13bd7caff69a697ebc290d689ba
Rafael Melo <rafaelmelo.programmer@gmail.com>
2024-08-06 21:17:04 -0300

    chore: implementing graceful shutdown

--- cmd/video_encoder/main.go
+++ cmd/video_encoder/main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-	"github.com/RafaLopesMelo/go-video-encoder/internal/infra/http/router"
+	"context"
+	"errors"
+	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+
+	"github.com/RafaLopesMelo/go-video-encoder/internal/infra/http/router"
 )
 
 func main() {
 	router.Setup()
-	http.ListenAndServe(":3000", nil)
+	srv := http.Server{
+		Addr: ":3000",
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("listen and serve returned err: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	err := srv.Shutdown(context.TODO())
+	if err != nil {
+		log.Fatalf("server shutdown failed: %v", err)
+	}
 }
